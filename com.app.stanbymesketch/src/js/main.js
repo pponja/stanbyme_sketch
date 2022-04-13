d625250c460638f137a4565be88ca76f950eb3f7
hyeseung.lee <hyeseung.lee@lge.com>
2022-04-13 17:24:12 +0900

    Add draw menu (line, circle, triangle, rectangle)

--- com.app.stanbymesketch/src/js/main.js
+++ com.app.stanbymesketch/src/js/main.js
@@ -5,6 +5,10 @@ const ctx = canvas.getContext('2d');
 let isDrawing = false;
 let currPos = {x:-1, y:-1};
 let lastPos = currPos;
+let shapeMode = "line"
+let startPos = {x:-1, y:-1};
+let shapes = [];
+let screens = [];
 
 // default setting
 let lineWidth = 2;
@@ -38,12 +42,38 @@ function handleDrawStart(event) {
         lastPos = getTouchPos(event)
     }
 
-    isDrawing = true
+    isDrawing = true;
+    ctx.beginPath();
+    startPos = lastPos;
+
+    // 1. get screen image
+    screens.push({screen:ctx.getImageData(0, 0, canvas.width, canvas.height)})
 }
 
 function handleDrawEnd() {
     isDrawing = false
-    lastPos = currPos = {x:-1, y:-1}
+
+    // 2. save drawing info in history array
+    if(shapeMode === "circle") {
+        shapes.push({
+            mode:shapeMode,
+            x:startPos.x + (currPos.x-startPos.x)/2,
+            y:startPos.y + (currPos.y-startPos.y)/2,
+            radius:(currPos.x-startPos.x)/2,
+            color: ctx.fillStyle})
+    } else if(shapeMode === "triangle") {
+
+    } else if(shapeMode === "rectangle") {
+        shapes.push({
+            mode:shapeMode,
+            x:startPos.x,
+            y:startPos.y,
+            width:currPos.x-startPos.x,
+            height:currPos.y-startPos.y,
+            color:ctx.fillStyle})
+    }
+
+    lastPos = currPos = startPos = {x:-1, y:-1}
     ctx.closePath()
 }
 
@@ -56,13 +86,84 @@ function handleDrawing(event) {
         currPos = getTouchPos(event)
     }
 
-    ctx.lineWidth = lineWidth;
-    ctx.strokeStyle = strokeStyle;
-    ctx.moveTo(lastPos.x, lastPos.y)
-    ctx.lineTo(currPos.x, currPos.y)
-    ctx.stroke()
-    
-    lastPos = currPos    
+    switch(shapeMode) {
+        case "line":
+            ctx.lineWidth = lineWidth;
+            ctx.strokeStyle = strokeStyle;
+            ctx.moveTo(lastPos.x, lastPos.y)
+            ctx.lineTo(currPos.x, currPos.y)
+            ctx.stroke();
+            shapes.push({
+                mode:shapeMode,
+                startX: lastPos.x,
+                startY: lastPos.y,
+                endX: currPos.x,
+                endY: currPos.y,
+                color: ctx.strokeStyle,
+                lineWidth: ctx.lineWidth
+            }) 
+            break;
+        case "circle":
+            drawAll()
+            ctx.beginPath();
+            let centerX = startPos.x + (currPos.x-startPos.x)/2;
+            let centerY = startPos.y + (currPos.y-startPos.y)/2;
+            let radius = (currPos.x-startPos.x)/2
+            ctx.arc(centerX, centerY, radius, 0, Math.PI*2, false)
+            ctx.stroke();
+            break;
+        case "triangle":
+            break;
+        case "rectangle":
+            drawAll()
+            ctx.strokeRect(startPos.x, startPos.y, currPos.x-startPos.x, currPos.y-startPos.y)
+            break;
+    }
+        
+    lastPos = currPos
+}
+
+// select draw menu
+function selectDraw() {
+    document.querySelector('.firstCtrlPanel').style.visibility = "hidden"
+    document.querySelector('.secondCtrlPanel').style.visibility = "visible"
+}
+
+// select shape
+function selectShape(shape) {
+    shapeMode = shape
+}
+
+// clear canvas and redraw all shapes
+function drawAll() {
+    // clear canvas
+    canvas.width = canvas.width;
+
+    // 1. draw screen image
+    if(screens.length > 0)
+        ctx.putImageData(screens[screens.length-1].screen, 0, 0)
+    /*
+    // 2. draw shapes in history array
+    for(var i=0; i<shapes.length; i++) {
+        var shape = shapes[i]
+        if(shape.mode === "line") {
+            ctx.lineWidth = shape.lineWidth;
+            ctx.strokeStyle = shape.color;
+            ctx.moveTo(shape.startX, shape.startY)
+            ctx.lineTo(shape.endX, shape.endY)
+            ctx.stroke();
+        } else if(shape.mode === "circle") {
+            ctx.beginPath();
+            ctx.fillStyle = shape.color
+            ctx.arc(shape.x, shape.y, shape.radius, 0, Math.PI*2, false)
+            ctx.stroke();
+        } else if(shape.mode === "triangle") {
+        } else if(shape.mode === "rectangle") {
+            ctx.fillStyle = shape.color
+            ctx.strokeRect(shape.x, shape.y, shape.width, shape.height)
+        }
+    }
+    */
 }
 
 // initial function
